a01d845a1b87a6008c28c6fa012f76cd95c04cc5
unknown <prabh.s2508@gmail.com>
2022-08-27 00:49:06 +0530

    adding files

---
+++ New folder/New folder/New folder/Weightloss-20220809T071053Z-001/Weightloss/countdown.js
@@ -0,0 +1,53 @@
+(function () {
+  const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+  //I'm adding this section so I don't have to keep updating this pen every year :-)
+  //remove this if you don't need it
+  let today = new Date(),
+      dd = String(today.getDate()).padStart(2, "0"),
+      mm = String(today.getMonth() + 1).padStart(2, "0"),
+      yyyy = today.getFullYear(),
+      nextYear = yyyy + 1,
+      dayMonth = "08/03/",
+      birthday = dayMonth + yyyy;
+  
+  today = mm + "/" + dd + "/" + yyyy;
+  if (today > birthday) {
+    birthday = dayMonth + nextYear;
+  }
+  //end
+  
+  const countDown = new Date(birthday).getTime(),
+      x = setInterval(function() {    
+
+        const now = new Date().getTime(),
+              distance = countDown - now;
+        h=Math.floor((distance % (day)) / (hour))+21
+        if(h>24)
+        {
+          d=1;
+          hn=h-24;
+        }
+        else{
+          d=0;
+          hn=h;
+        }
+        document.getElementById("days").innerText = Math.floor(distance / (day))+d,
+          // document.getElementById("hours").innerText = Math.floor((distance % (day)) / (hour))+21,
+          document.getElementById("hours").innerText=hn,
+          document.getElementById("minutes").innerText = Math.floor((distance % (hour)) / (minute)),
+          document.getElementById("seconds").innerText = Math.floor((distance % (minute)) / second);
+
+        //do something later when date is reached
+        if (distance < 0) {
+          document.getElementById("headline").innerText = "Registration Closed!";
+          document.getElementById("countdown").style.display = "none";
+          document.getElementById("content").style.display = "block";
+          clearInterval(x);
+        }
+        //seconds
+      }, 0)
+  }());
